6245c619429ee333feaacc97d8a402b940694bbc
bala-amypo <bala@amypo.in>
2025-12-25 08:38:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RelationshipDeclarationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RelationshipDeclarationServiceImpl.java
@@ -9,6 +9,7 @@ import com.example.demo.service.RelationshipDeclarationService;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class RelationshipDeclarationServiceImpl implements RelationshipDeclarationService {
 
     private final RelationshipDeclarationRepository repository;
