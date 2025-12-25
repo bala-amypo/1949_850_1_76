505bd6f36a656b8d6235624d051fdd1e9518362c
bala-amypo <bala@amypo.in>
2025-12-25 13:38:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/ConflictFlagRepository.java
+++ src/main/java/com/example/demo/repository/ConflictFlagRepository.java
@@ -2,16 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ConflictFlag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface ConflictFlagRepository
-        extends JpaRepository<ConflictFlag, Long> {
+public interface ConflictFlagRepository extends JpaRepository<ConflictFlag, Long> {
 
-    List<ConflictFlag> findByCaseId(Long caseId);
+    List<ConflictFlag> findByConflictCaseId(Long conflictCaseId);
 }
-
-
-
