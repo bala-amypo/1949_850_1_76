77ac9743b5ece329b8a056faadfa359685bd1add
bala-amypo <bala@amypo.in>
2025-12-25 13:44:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/PersonProfileController.java
+++ src/main/java/com/example/demo/controller/PersonProfileController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.model.PersonProfile;
 import com.example.demo.service.PersonProfileService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
