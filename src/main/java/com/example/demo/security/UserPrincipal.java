1278bd0cfda48aba6e948f54c0300922f1d2623c
bala-amypo <bala@amypo.in>
2025-12-26 09:04:47 +0000

    User commit

--- src/main/java/com/example/demo/security/UserPrincipal.java
+++ src/main/java/com/example/demo/security/UserPrincipal.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
+import java.util.List;
 
 public class UserPrincipal implements UserDetails {
     private Long id;
